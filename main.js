1bcd01642991a7a9d9a8330930a8fa0a076b3562
8ark60 <8ark6young@gmail.com>
2022-09-23 17:10:21 +0900

    JS: Navbar event

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 'use strict';
 
-// scroll Navbar Event
+// 스크롤 시 Navbar 이벤트
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', function(){
-    console.log(window.scrollY);
-    console.log(`NavbarHeight: ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`NavbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// 메뉴 클릭 시 해당 곳으로 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', function(event){
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
